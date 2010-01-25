86aff690d16c01ef4c936fedf7c1e173c81daf5a
Kevin Lynch <klynch@gmail.com>
2010-01-24 22:59:45 -0500

    time script

---
+++ assn1/time.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ITERATIONS=10
+
+export LD_LIBRARY_PATH=/site/local/papi_x86_64/lib:${HOME}/local/fftw-2.1.5-kodiak
+for fft in 4 5 6 7; do
+    n=1
+    while [ $n -lt 25 ]; do
+        ./src/time_fft $n $ITERATIONS $fft $test
+        let n+=1
+    done
+done
