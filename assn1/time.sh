106b95dc5c46ee93f16e174596c59ac3dd331166
Kevin Lynch <klynch@gmail.com>
2010-01-24 23:13:19 -0500

    run issues

--- assn1/time.sh
+++ assn1/time.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 ITERATIONS=10
+TEST=0
 
 export LD_LIBRARY_PATH=/site/local/papi_x86_64/lib:${HOME}/local/fftw-2.1.5-kodiak
 for fft in 4 5 6 7; do
     n=1
     while [ $n -lt 25 ]; do
-        ./src/time_fft $n $ITERATIONS $fft $test
-        $n+=1
+        ./src/time_fft $n $ITERATIONS $fft $TEST 
+        n=$(( $n + 1))
     done
 done
