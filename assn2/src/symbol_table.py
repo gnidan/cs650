40895646fee7c6c9a8725f0c3f7464b8b0c19bdd
George DAndrea <gnd25@ws32.cs.drexel.edu>
2010-02-13 01:13:46 -0500

    Moved notes to be markdown files

--- assn2/src/symbol_table.py
+++ assn2/src/symbol_table.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python
 
+class Variable:
+  """Maintains the record for a particular variable in our symbol table"""
+  access = []
+
+  def __init__(self):
+
+  def access(self, icode):
+    """Record when a particular icode expression access this variable"""
+    access.append(icode)
+
+class Scalar(Variable):
+  """Represents a scalar value"""
+  value = 0
+
+class Vector(Variable):
+  """Represents a vector of scalars"""
+  size = 0
+  scalars = []
+
+class Expression(Variable):
+  """Represents an SPL Expression tree"""
+  input_size = 0
+  output_size = 0
+  spl_expression = None
+
+  def __init__(self, spl_expression):
+    self.spl_expression = spl_expression
+
+  def generate_icode(input, output):
+    return spl_expression.generate_icode(input, output)
+
 class SymbolTable:
   vectors = {}
   scalars = {}
@@ -8,4 +39,3 @@ class SymbolTable:
   def define(name, spl_expression):
     expressions[name] = Expression(spl_expression)
     expressions.add(name)
-
