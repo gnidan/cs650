9a9c3bc0ea41fc9ecc836ebab0a4fa43a14187c7
George DAndrea <gnd25@ws32.cs.drexel.edu>
2010-02-13 02:57:18 -0500

    Fleshed out symbol table class, added Symbol Collection, did some cleanup of types

--- assn2/src/symbol_collection.py
+++ assn2/src/symbol_collection.py
@@ -2,22 +2,31 @@
 
 class Variable:
   """Maintains the record for a particular variable in our symbol table"""
-  access = []
 
   def __init__(self):
+    self.access = []
 
   def access(self, icode):
     """Record when a particular icode expression access this variable"""
-    access.append(icode)
+    self.access.append(icode)
 
 class Scalar(Variable):
   """Represents a scalar value"""
-  value = 0
+  pass
+
+class Int(Scalar):
+  def __init__(self):
+    self.value = 0
+
+class Float(Scalar):
+  def __init__(self):
+    self.value = 0
 
 class Vector(Variable):
   """Represents a vector of scalars"""
-  size = 0
-  scalars = []
+  def __init__(self, size):
+    self.size = size
+    self.scalars = [Scalar()] * size #initialize vector to have a given size
 
 class Expression(Variable):
   """Represents an SPL Expression tree"""
@@ -31,11 +40,41 @@ class Expression(Variable):
   def generate_icode(input, output):
     return spl_expression.generate_icode(input, output)
 
-class SymbolTable:
-  vectors = {}
-  scalars = {}
-  expressions = {}
+class SymbolCollection:
+  def __init__(self, input_size, output_size):
+    self.r       = []
+    self.f       = []
+    self.i       = []
+    self.p       = []
+    self.t       = []
+    self.x       = Vector(input_size)
+    self.y       = Vector(output_size)
+    self.sym_tab = SymbolTable()
 
-  def define(name, spl_expression):
-    expressions[name] = Expression(spl_expression)
-    expressions.add(name)
+class SymbolTable:
+  def __init__(self):
+    dict = {} # TODO redefine this to be set to the dict containing all
+              # the predefined values
+    self.depth = 0
+    self.table = [dict]
+
+  def __getitem__(self, key):
+    dict = self.table[self.depth]
+    return dict[key]
+
+  def __setitem__(self, key, value):
+    dict = self.table[self.depth]
+    dict[name] = value
+
+  def __delitem__(self, key):
+    dict = self.table[self.depth]
+    del dict[name]
+
+  def append_scope(self):
+    dict = self.table[self.depth]
+    self.table.append(dict.copy())
+    depth += 1
+
+  def pop_scope(self):
+    self.table.pop()
+    depth -= 1
