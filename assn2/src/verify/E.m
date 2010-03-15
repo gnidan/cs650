0be5e645fdf0129ae9acb2e55c8b034592b48c83
Kevin Lynch <klynch@gmail.com>
2010-03-15 15:17:23 -0400

    getting closer to a clean implementation for Raders

---
+++ assn2/src/verify/E.m
@@ -0,0 +1,4 @@
+function a = E(D)
+a = direct_sum(1, D);
+a(1,2) = 1;
+a(2,1) = 1;
--- assn2/src/verify/rader.m
+++ assn2/src/verify/rader.m
@@ -1,29 +1,15 @@
 %Must implement:
-%CONJ_TRANS
+%CONJ_TRANS -- this is just a conjugate(transpose(A))... no need for new symbol
 %TRANSPOSE (only deals with scalars... can probably safely be the same as CONJ_TRANS)
 %G
-%WG --
-%D -- can be SPL or icode
+%WG
 %E -- should take SPL as input, call it, and add the 3 1's.
 function ans = rader(n, g1, g2)
 
-%0, 1, 3 4 2
-%G1 = [ 1 0 0 0 0; 0 1 0 0 0; 0 0 0 1 0; 0 0 0 0 1; 0 0 1 0 0 ];
+DFT1 = direct_sum( 1, transpose(conj((F(n-1)))) );
 
-DFT1 = direct_sum( 1, conj_trans(F(n-1)) );
-
-%The D Matrix takes as parameters g1 and F
-D = diagonal( scale(compose( F(n-1), WG(n, g1) ), 1/(n-1)));
-
-%This should be in icode... easy to write up!
-E = direct_sum(1, D);
-E(1,2) = 1;
-E(2,1) = 1;
+e = E(diagonal(scale(compose( F(n-1), WG(n, g1) ), 1/(n-1))) );
 
 DFT2 = direct_sum(1, F(n-1));
 
-%0, 1, 2, 4, 3
-%G2 = [1 0 0 0 0; 0 1 0 0 0; 0 0 1 0 0; 0 0 0 0 1; 0 0 0 1 0];
-
-ans = compose(rpermutation(G(n, g1)), compose(DFT1, compose(E, compose(DFT2, permutation(G(n, g2))))));
-%ans=ws;
+ans = compose(rpermutation(G(n, g1)), compose(DFT1, compose(e, compose(DFT2, permutation(G(n, g2))))));
