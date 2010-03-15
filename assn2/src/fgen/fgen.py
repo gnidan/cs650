4a0c49dbf22282898e75befb124b206dd23b6da6
Kevin Lynch <klynch@gmail.com>
2010-03-15 15:51:04 -0400

    fgen stuff

--- assn2/src/fgen/fgen.py
+++ assn2/src/fgen/fgen.py
@@ -15,6 +15,7 @@ import sys
 import getopt
 import math
 from factor import *
+from memoize import memoize
 
 help_message = '''
 This is the CS650 SPL compiler in PLY
@@ -24,10 +25,33 @@ This is the CS650 SPL compiler in PLY
     -o file, --output=file  The SPL output file
 '''
 
+def CTm(r, s):
+  return "compose(tensor(F({r}),I({s})),compose(T({n},{s}),compose(tensor(I({r}),F({s})),L({n},{r}))))".format(r=r,s=s,n=r*s)
+
+def CT(r, s):
+  return "(compose (tensor (F {r}) (I {s})) (compose (T {n} {s}) (compose (tensor (I {r}) (F {s})) (L {n} {r}))))".format(r=r,s=s,n=r*s)
+
+def PF():
+  pass
+
+def Rader():
+  pass
+
+@memoize
 def fgen(n, verbose=False):
-	print isprime(n)
-	print factors(n)
-	print primefactors(n)
+  facts = factors(n)
+  if n < 8:
+    return "(F %d)" % (n)
+  elif isprime(n):
+    print fgen(n-1)
+    print fgen(n-1)
+    return "(Rader %d) -> %d %d" % (n, n-1, n-1)
+  else:
+    m = facts[len(facts)/2]
+    k = n/m
+    print fgen(m)
+    print fgen(k)
+    return "(CT %d) -> %d %d" % (n, m, k)
 
 class Usage(Exception):
 	def __init__(self, msg):
@@ -42,10 +66,10 @@ def main(argv=None):
 			opts, args = getopt.getopt(argv[1:], "ho:vn:", ["help", "output="])
 		except getopt.error, msg:
 			raise Usage(msg)
-		
+
 		verbose = False
 		size = 32
-		
+
 		# option processing
 		for option, value in opts:
 			if option == "-v":
@@ -56,14 +80,14 @@ def main(argv=None):
 				raise Usage(help_message)
 			if option in ("-o", "--output"):
 				output = value
-	
+
 	except Usage, err:
 		print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
 		print >> sys.stderr, "\t for help use --help"
 		return 2
-	
+
 	print verbose, size
-	fgen(size, verbose)
+	print fgen(size, verbose)
 
 
 if __name__ == "__main__":
